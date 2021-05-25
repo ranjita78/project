8e541f45a43fa3cb2f01c91ed21ca915f4aa28a1
ranjita78 <ranjeetaachary@gmail.com>
2021-05-25 11:35:47 +0530

    subtotal

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,7 @@ class Order < ApplicationRecord
 
 
     def subtotal
-        order_items.collect{|order_item| order_item.valid? ? order_item.unit_price*2 *order_item.quantity : 0}.sum
+        order_items.collect{|order_item| order_item.valid? ? order_item.unit_price*order_item.quantity : 0}.sum
     end
 
 
